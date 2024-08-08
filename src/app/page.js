172fb62c03b32f6358bf88630137f5b1a395b7d2
tigocode <tiago.dantas16@live.com>
2024-08-08 00:17:55 -0300

    terceira versao do código, com refatoracao com objeto de reduzir linhas do código

--- src/app/page.js
+++ src/app/page.js
@@ -4,23 +4,19 @@ import styles from './page.module.css';
 
 export default function Home() {
   const [ status, setStatus ] = useState(false);
-  const [ cordoFundo, setCordoFundo ] =  useState(styles.bg_vermelho);
+
+  const fundoVemerlho = styles.bg_vermelho;
+  const fundoAzul = styles.bg_azul;
 
   function alterarEstilo() {
-    if(status === false) {
-      setCordoFundo(styles.bg_azul);
-      setStatus(true);
-    } else {
-      setCordoFundo(styles.bg_vermelho);
-      setStatus(false);
-    }
+    setStatus(!status);
   };
 
   return (
     <main className={styles.main}>
       <div className={styles.container}>
         <h2>Alterando estilo do elemento</h2>
-        <div className={cordoFundo}></div>
+        <div className={status ? fundoAzul: fundoVemerlho}></div>
         <p>Clique no botão para alterar o estilo do elemento acima.</p>
         <button onClick={alterarEstilo}>Alterar</button>
       </div>
